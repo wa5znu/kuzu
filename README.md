32e4ec37244f0cc24bb1ce98f9ebb2b219abb62c
Leigh Klotz <klotz@klotz.me>
2022-06-05 17:14:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Small Dust Sensor Display
+
+![](docs/dust.jpg | width=250)
+
+I got an very small board, an ESP32-C3 with 0.42 inch SSD1306 72x48 pixel display, the
+QWIC/Stemma type 4-pin connector, USB-C power/serial/programming, and
+a Neopixel-like LED.  They have them in RP2040 as well, and I have one of those on order.
+
+I'm using Arduino IDE for this, but I think there's a MicroPython distribution for it as well, just as there is for the RP2040-based one.
+Unlike the RP2040, though, the ESP32-C3 includes WiFi and BLE, both useful for projects around the house.
+
+The LED was a painfully blue from the start, so I brought in the
+Adafruit NeoPixel library to cut it back, using the Adafruit blink
+example to start with.
+
+I used the manufacturer-recommended 01Space Graphics Test sketch to start, then once I'd settled issues and gotten a working environment, I moved to the examples below and connected to my own internal MQTT server.
+
+# Provenance
+Cobbled together from these:
+- https://github.com/01Space/ESP32-C3-0.42LCD.git
+- https://github.com/survivingwithandroid/ESP32-MQTT.git
+
+# Issues
+- Set CDC on Boot in Arduino IDE Menu to make Serial.print work
+- Direct USB-C cable to Linux desktop worked best
+- Multiple copies of u8g2 and u8g2_arduino installed with different fonts
+- As always, string parsing in C
+- Can't find a font size that supports more than four key-value lines on LCD
+
+# References
+- https://www.banggood.com/ESP32-C3-0_42-inch-LCD-Development-Board-RISC-V-WiFi-Bluetooth-Arduino-or-Micropython-p-1947665.html
+- https://github.com/01Space/ESP32-C3-0.42LCD
+- https://www.electronics-lab.com/getting-started-with-espressifs-esp32-c3-devkitm-1-on-arduino-ide/
+- https://www.cnx-software.com/2022/05/17/esp32-c3-0-42lcd-is-a-tiny-wifi-ble-iot-board-with-0-42-inch-display-qwiic-connector/
+- https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/boards/ESP32-C3-DevKitM-1.html
+- https://learn.adafruit.com/adafruit-qt-py-esp32-c3-wifi-dev-board/arduino-neopixel-blink
+- https://github.com/survivingwithandroid/ESP32-MQTT/
+- https://www.survivingwithandroid.com/getting-started-with-esp32-build-a-weather-station/
+- https://github.com/platformio/platform-espressif32/issues/591 
+- https://github.com/olikraus/u8g2/wiki/u8g2reference
+- https://www.best-microcontroller-projects.com/ssd1306.html
+- https://github.com/olikraus/u8g2/wiki/fntlist8
---
+++ kuzu.ino
@@ -0,0 +1,204 @@
+/**
+ * kuzo - small dust sensor display
+ *
+ * Copyright 2022 Leigh L. Klotz, Jr. <leigh@wa5znu.org>
+ * MIT License - see file LICENSE
+ * Based on code examples:
+ * https://github.com/01Space/ESP32-C3-0.42LCD.git
+ * https://github.com/survivingwithandroid/ESP32-MQTT.git
+ */
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+#define SDA_PIN 5
+#define SCL_PIN 6
+
+#include "secrets.h"
+
+long last_time = 0;
+char data[100];
+
+// MQTT client
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient); 
+
+// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
+U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);   // EastRising 0.42" OLED
+
+void connectToWiFi() {
+  u8g2.clearBuffer();
+  u8g2.clearDisplay();
+  u8g2.sendBuffer();
+
+  Serial.print("Connecting: ");
+  u8g2.drawStr(0, 0, "Connecting");
+  u8g2.sendBuffer();
+
+  WiFi.begin(SSID, PWD);
+  Serial.println(SSID);
+  u8g2.drawStr(0, 10, SSID);
+  u8g2.sendBuffer();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  {
+    const char *ip = WiFi.localIP().toString().c_str();
+    u8g2.clearBuffer();
+    u8g2.clearDisplay();
+    Serial.printf("\nConnected %s\n", ip);
+    Serial.printf("IP: %s\n", ip);
+    u8g2.drawStr(0, 20, ip);
+    u8g2.sendBuffer();
+  }
+
+} 
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char buf[256];
+  Serial.printf("* topic %s\n", topic);
+  u8g2.clearBuffer();
+  u8g2.clearDisplay();
+  memset(buf, 0, sizeof(buf));
+  memcpy(buf, payload, min(length, sizeof(buf)-1));
+  Serial.println(buf);
+  int item_no = 0;
+  char *last_start = buf;
+  for (int i = 0; i < sizeof(buf); i++) {
+    // pm01=0;pm2_5=1;pm10=1;aqi=4;pm2_5raw=0
+    char c = buf[i];
+    if (c == '_') {
+      buf[i] = '.';
+    } else if (c == '=') {
+      buf[i] = '\0';
+    } else if (c == ';' || c == '\r' || c == '\n') {
+      buf[i] = '\0';
+    } else {
+      buf[i] = toupper(c);
+    }
+  }
+
+  const int ITEM_MAX=8;
+  for (char *last_start = buf; last_start[0] != '\0'; last_start += strlen(last_start)+1) {
+    int x = (item_no % 2) * 44;	// 36 is half, but numbers are shorter than the labels
+    int y = (item_no / 2) * 10;
+    Serial.printf("* item '%s' item_no = %d x=%d y=%d last_start='%s'\n", last_start, item_no, x, y, last_start);
+    u8g2.drawStr(x, y, last_start);
+    u8g2.sendBuffer();
+    if (++item_no >= ITEM_MAX) {
+      Serial.println("* truncating items");
+      break;
+    }
+  }
+}
+
+void setupMQTT() {
+  mqttClient.setServer(mqttServer, mqttPort);
+  mqttClient.setCallback(callback);
+}
+
+
+void setup() {
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  
+  u8g2_prepare();
+
+  connectToWiFi();
+
+#if 0
+  setup_bme();
+#endif
+
+  setupMQTT();
+}
+
+void reconnect() {
+  Serial.println("Connecting to MQTT Broker...");
+  while (!mqttClient.connected()) {
+      Serial.println("Reconnecting to MQTT Broker..");
+      String clientId = "ESP32Client-";
+      clientId += String(random(0xffff), HEX);
+      
+      if (mqttClient.connect(clientId.c_str())) {
+        Serial.println("Connected.");
+        // subscribe to topic
+        mqttClient.subscribe(mqttTopic);
+      }
+      
+  }
+}
+
+void loop() {
+  if (!mqttClient.connected())
+    reconnect();
+  mqttClient.loop();
+#if 0
+  bmePublish();
+#endif
+
+}
+
+void u8g2_prepare(void) {
+  u8g2.begin();
+  // u8g2.setFont(u8g2_font_6x10_tf);
+  // u8g2.setFont();
+  u8g2.setFont(
+	       // u8g2_font_5x8_tf
+	       u8g2_font_BBSesque_tf
+  );
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+}
+
+// BME sensor publishing - from mqtt project
+// ordered a sensor just to check it out
+
+#if 0
+void bme_setup() {
+  if (!bme.begin(0x76)) {
+    Serial.println("Problem connecting to BME280");
+  }
+}
+
+void bmePublish() {
+  long now = millis();
+  if (now - last_time > 60000) {
+    // Send data
+    float temp = bme.readTemperature();
+    float hum = bme.readHumidity();
+    float pres = bme.readPressure() / 100;
+    
+
+    // Publishing data throgh MQTT
+    sprintf(data, "%f", temp);
+    Serial.println(data);
+    mqttClient.publish("/swa/temperature", data);
+    sprintf(data, "%f", hum);
+    Serial.println(hum);
+    mqttClient.publish("/swa/humidity", data);
+    sprintf(data, "%f", pres);
+    Serial.println(pres);
+    mqttClient.publish("/swa/pressure", data);
+    last_time = now;
+  }
+}
+#endif
